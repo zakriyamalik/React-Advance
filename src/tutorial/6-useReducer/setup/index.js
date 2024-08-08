cddb24abd79f7f1fe6e7f6606d2015b826edcd6e
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-08-08 23:01:36 +0500

    "Updated App.js to import Setup from tutorial/6-useReducer/setup, and modified files in tutorial/5-useRef/setup and tutorial/6-useReducer/setup to add functionality to useRef and useReducer examples."

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,9 +2,93 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer=(state,action)=>{
+  if(action.type==="TESTING")
+  {
+    const newItems = [...state.people,action.payload]
+    return {...state,
+      people:newItems,
+      modalContent: 'item added'
+     ,isModalOpen:true
+  
+    }
+  }
+  if(action.type==="NO_VALUE")
+    {
+      return {...state, modalContent: 'please enter value',isModalOpen:true}
+    }
+    if(action.type==="CLOSE_MODAL")
+      return {...state, isModalOpen:false}
+    if(action.type==='REMOVE_ITEM')
+    {
+      const newItems = state.people.filter(item=>item.id!==action.payload)
+      return {...state,people:newItems}
+    }
 
+}
+const defaultState={
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+}
 const Index = () => {
-  return <h2>useReducer</h2>;
+  
+  const [name,setName]=useState('');
+  const [state,dispatch]= useReducer(reducer,defaultState);
+ 
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({type:'TESTING',payload:newItem})
+    
+    }  else {
+      console.log('empty values');
+      dispatch({type:'NO_VALUE',payload:'No value'})
+      setName('');
+      // setModalContent('No value')
+      // setIsModalOpen(true)
+     
+    }
+   
+  };
+  const closeModal=()=>{
+    dispatch({type:'CLOSE_MODAL',payload:''})
+  }
+
+  return(
+    <>
+    {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent} />}
+    <form onSubmit={handleSubmit} className='form'>
+      <div>
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder='Enter your name'
+      />
+      </div>
+      <button type='submit' >Add</button>
+    </form>
+    {
+      state.people.map((person) => {
+        return (
+          <div key={person.id} className='item'>
+            <h4>{person.name}</h4>
+            <button onClick={()=>
+              dispatch({type:'REMOVE_ITEM',payload:person.id})
+            }>
+remove
+            </button>
+          </div>
+        );
+      })
+    }
+    </>
+
+
+  );
 };
 
 export default Index;
